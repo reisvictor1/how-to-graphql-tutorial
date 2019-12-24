1000e39ad27165fcdf36ad15f99285aceedd9320
reisvictor1 <reisvictor448@gmail.com>
2019-12-24 16:19:30 -0300

    Adicionado queries e mutations

--- server/src/index.js
+++ server/src/index.js
@@ -1,22 +1,71 @@
 const { GraphQLServer } = require('graphql-yoga')
 
-// 1
-const typeDefs = `
-type Query {
-  info: String!
+let links = [{
+    id: 'link-0',
+    url: 'www.favurl.com.br',
+    description: 'My favorite URL'
+},{
+    id: 'link-1',
+    url: 'www.howtographql.com',
+    description: 'Fullstack tutorial for GraphQL'
 }
-`
+]
 
-// 2
+let idCount = links.length
+
 const resolvers = {
   Query: {
-    info: () => `This is the API of a Hackernews Clone`
-  }
+    info: () => "Welcome!!",
+    feed: () => links,
+    link: (parent,args) => {
+
+        const idLink = args.id;
+        const chosen = idLink[idLink.length - 1];
+        return links[chosen];
+          
+    }
+  },
+  Mutation: {
+      post: (parent,args) => {
+          const link = {
+              id: `link-${idCount++}`,
+              description: args.description,
+              url: args.url
+          }
+          links.push(link)
+          return link;
+
+      },
+      updateLink: (parent,args) => {
+        const idLink = args.id;
+        const idchosen = idLink[idLink.length - 1];
+
+        if(args.url != null)
+          links[idchosen].url = args.url;
+        
+        if(args.description != null)
+          links[idchosen].description = args.description;
+
+      },
+      deleteLink: (parent,args) => {
+        
+        const idLink = args.id;
+        const idchosen = idLink[idLink.length - 1];
+
+        if(idchosen > -1)
+          links.splice(idchosen,1);
+
+
+          idCount--;
+        
+      }
+     
+  },
 }
 
 // 3
 const server = new GraphQLServer({
-  typeDefs,
+  typeDefs: './src/schema.graphql',
   resolvers,
 })
 server.start(() => console.log(`Server is running on http://localhost:4000`))
